cb3537cd9b715bf0e1cce98e099241a36a72c788
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-06-13 15:51:41 -0300

    Add files via upload

--- ubuinst3.sh
+++ ubuinst3.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cat /dev/null > ~/.bash_history && history -c
+rm /bin/ubuinst* > /dev/null 2>&1
 echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgcmF3LmdpdGh1YnVzZXJjb250ZW50LmNv
 bS9uYW5kb3NsYXllci9pbnN0YWxsL2JhZC91YnVpbnN0Mi5zaCAmJiBjaG1vZCAreCB1YnVpbnN0
 Mi5zaCAmJiBkb3MydW5peCB1YnVpbnN0Mi5zaCA+IC9kZXYvbnVsbCAyPiYxCmNkIHx8IGV4aXQ=' | base64 -d | bash
